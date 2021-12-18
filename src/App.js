293d612c990012377a8d7872150fd3369cb7decf
Mkseey <kartikmahurkar752@gmail.com>
2021-12-18 19:23:38 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,122 @@
-import logo from './logo.svg';
-import './App.css';
+import "./style.scss";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="row">
+        <div>
+          <header className="header justify-content-end">
+            <div className="dropdown">
+              <text className="header-text">Year Ending</text>
+              <button className="btn dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                VASY
+              </button>
+              <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                <a className="dropdown-item" href="#">Action</a>
+                <a className="dropdown-item" href="#">Another action</a>
+                <a className="dropdown-item" href="#">Something else here</a>
+              </div>
+            </div>
+          </header>
+        </div>
+        <div className="col-3">
+          <div className="details-card blue-card">
+            <div className="details-card__title">
+              activity card
+            </div>
+            <div className="details-card__body">
+              <div className="count-no">
+                100
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col-3">
+          <div className="details-card blue-card">
+            <div className="details-card__title">
+              activity card
+            </div>
+            <div className="details-card__body">
+              <div className="count-no">
+                200
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col-3">
+          <div className="details-card blue-card">
+            <div className="details-card__title">
+              activity card
+            </div>
+            <div className="details-card__body">
+              <div className="count-no">
+                300
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col-3">
+          <div className="details-card blue-card"  >
+            <div className="details-card__title">
+
+            </div>
+            <div className="details-card__body">
+              <div className="count-no">
+
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col-3">
+          <div className="details-card blue-card">
+            <div className="details-card__title">
+              activity card
+            </div>
+            <div className="details-card__body">
+              <div className="count-no">
+                500
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col-3">
+          <div className="details-card blue-card">
+            <div className="details-card__title">
+              activity card
+            </div>
+            <div className="details-card__body">
+              <div className="count-no">
+                600
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col-3">
+          <div className="details-card blue-card">
+            <div className="details-card__title">
+              activity card
+            </div>
+            <div className="details-card__body">
+              <div className="count-no">
+                700
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col-3">
+          <div className="details-card blue-card">
+            <div className="details-card__title">
+              activity card
+            </div>
+            <div className="details-card__body">
+              <div className="count-no">
+                800
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      hello
     </div>
   );
 }
